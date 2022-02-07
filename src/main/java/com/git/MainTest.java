61076c3201f187dedf9129a7bc829c5d0ffc38e1
hasse <hasse@qq.com>
2022-02-07 11:28:30 +0800

    second,commit

--- src/main/java/com/git/MainTest.java
+++ src/main/java/com/git/MainTest.java
@@ -3,5 +3,7 @@ package com.git;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("first,commit");
+        System.out.println("2,commit");
+        System.out.println("3,commit");
     }
 }
